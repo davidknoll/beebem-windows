2e2b54c29636e785209d9c8fe2a3c6ed32f28d3b
David Knoll <david@davidknoll.me.uk>
2018-02-25 04:14:04 +0000

    First attempt at I/O passthrough

--- Src/idepassthru.cpp
+++ Src/idepassthru.cpp
@@ -1,9 +1,64 @@
 /* 16-bit IDE host passthru support for BeebEm, by David Knoll */
 
+#include <windows.h>
 #include "idepassthru.h"
 
-bool IDEPassThruEnabled = false;
+static void (*pOut32)(short, short);
+static short (*pInp32)(short);
+static bool (*pIsInpOutDriverOpen)(void);
 
-void PTWrite(int addr, int data) { return; }
-int PTRead(int addr) { return 0xFF; }
-void PTReset(void) { return; }
+bool IDEPassThruEnabled = false;
+static int inlatch, outlatch;
+
+void PTWrite(int addr, int data)
+{
+	int outptmp;
+	if (!pIsInpOutDriverOpen) return;
+	if (!(*pIsInpOutDriverOpen)()) return;
+
+	if (addr & 0x8) {
+		if (addr & 0x4) {
+			// IDE /CS1
+			(*pOut32)(0x0370 | (addr & 0x7), data & 0xFF);
+		} else {
+			// '646 latch
+			outlatch = data & 0xFF;
+		}
+	} else {
+		// IDE /CS0
+		outptmp = (outlatch << 8) | (data & 0xFF);
+		(*pOut32)(0x0170 | (addr & 0x7), outptmp);
+	}
+}
+
+int PTRead(int addr)
+{
+	int inptmp;
+	if (!pIsInpOutDriverOpen) return 0xFF;
+	if (!(*pIsInpOutDriverOpen)()) return 0xFF;
+
+	if (addr & 0x8) {
+		if (addr & 0x4) {
+			// IDE /CS1
+			return (*pInp32)(0x0370 | (addr & 0x7)) & 0xFF;
+		} else {
+			// '646 latch
+			return inlatch;
+		}
+	} else {
+		// IDE /CS0
+		inptmp = (*pInp32)(0x0170 | (addr & 0x7));
+		inlatch = (inptmp >> 8) & 0xFF;
+		return inptmp & 0xFF;
+	}
+}
+
+void PTReset(void)
+{
+	HINSTANCE hInpOutDll = LoadLibrary("inpout32.dll");
+	if (hInpOutDll) {
+		pOut32 = (void (*)(short, short)) GetProcAddress(hInpOutDll, "Out32");
+		pInp32 = (short (*)(short)) GetProcAddress(hInpOutDll, "Inp32");
+		pIsInpOutDriverOpen = (bool (*)(void)) GetProcAddress(hInpOutDll, "IsInpOutDriverOpen");
+	}
+}
